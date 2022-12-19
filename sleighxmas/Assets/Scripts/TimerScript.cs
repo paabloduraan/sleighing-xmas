7315ead0f6f47f18744c8b46832c2e24feb7d3c9
paabloduraan <pablo.duran@tufts.edu>
2022-12-19 13:29:42 -0500

    added timer
    added a timer to level 3

--- sleighxmas/Assets/Scripts/TimerScript.cs
+++ sleighxmas/Assets/Scripts/TimerScript.cs
@@ -1,16 +1,23 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class TimerScript : MonoBehaviour
 {
     public float TimeLeft;
     public bool TimerOn = false;
 
-    public Text TimerTxt;
+    public GameObject number1;
+    public GameObject number2;
+    public GameObject number3;
+
+    public Image divider;
 
     void Start()
     {
+        divider.enabled = false;
         TimerOn = true;
+        Time.timeScale = 0f;
     }
 
     void Update()
@@ -19,14 +26,18 @@ public class TimerScript : MonoBehaviour
         {
             if(TimeLeft > 0)
             {
-                TimeLeft -= Time.deltaTime;
+                TimeLeft -= Time.unscaledDeltaTime;
                 updateTimer(TimeLeft);
             }
             else
             {
-                Debug.Log("Time is UP!");
                 TimeLeft = 0;
                 TimerOn = false;
+                number1.SetActive(false);
+                number2.SetActive(false);
+                number3.SetActive(false);
+                divider.enabled = true;
+                Time.timeScale = 1f;
             }
         }
     }
@@ -34,11 +45,24 @@ public class TimerScript : MonoBehaviour
     void updateTimer(float currentTime)
     {
         currentTime += 1;
-
         float minutes = Mathf.FloorToInt(currentTime / 60);
         float seconds = Mathf.FloorToInt(currentTime % 60);
 
-        TimerTxt.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        if (seconds == 3) {
+            number1.SetActive(false);
+            number2.SetActive(false);
+            number3.SetActive(true);
+        }
+        else if (seconds == 2) {
+            number1.SetActive(false);
+            number2.SetActive(true);
+            number3.SetActive(false);
+        }
+        else if (seconds == 1) {
+            number1.SetActive(true);
+            number2.SetActive(false);
+            number3.SetActive(false);
+        }
     }
 
 }
